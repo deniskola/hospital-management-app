6f2f50b7f7c2a9c24c8783a7ddf4c2515590395a
ilirianthaci18 <ilirianthaqi@live.com>
2021-04-04 15:17:10 +0200

    Sidebar

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import * as s from './App.styles';
+import * as Palette from './colors'
+
+// Components
+import Sidebar from './Components/Sidebar/Sidebar';
+import MainView from './Components/MainView/MainView'
+
+
+const App = () => {
+
+  const sidebarHeader = {
+    fullName: 'Hospital X',
+    shortName: 'X'
+  };
+  const menuItems = [
+    {name: 'Dashboard', to: '/', icon: '/icons/dashboard.png', subMenuItems: [] },
+    {name: 'Activity', to: '/activity', icon: '/icons/activity.png', subMenuItems: [] },
+    {name: 'Appointments', to: '/appointments', icon: '/icons/appointment.png', subMenuItems: [] },
+    {name: 'Profile',to:'/profile',icon:'/icons/profile.png',subMenuItems:[]},
+    {name: 'About', to: '/about', icon: '/icons/aboutus.png', subMenuItems: [] },
+    {name: 'Services', to: '/services', icon: '/icons/services.png', subMenuItems: [] },
+    {name: 'Contact Us', to: '/contacts', icon: '/icons/contactus.png', subMenuItems: [] }
+  ];
+
+  const fonts = {
+    header: 'ZCOOL KuaiLe',
+    menu: 'Poppins'
+  }
+
+  return (
+    <s.App>
+      <Sidebar
+        sidebarHeader={sidebarHeader}
+        menuItems={menuItems}
+        fonts={fonts}
+        colorPalette={Palette.silver}
+      />
+      <MainView />
+    </s.App>
+  );
+}
+
+export default App;
---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// Components
+import Dashboard from './Components/MainView/Dashboard/Dashboard';
+import Activity from './Components/MainView/Activity/Activity';
+import Appointments from './Components/MainView/Appointments/Appointments';
+import About from './Components/MainView/About/About';
+import Services from './Components/MainView/Services/Services';
+import Contacts from './Components/MainView/Contacts/Contacts';
+import Profile from './Components/MainView/Profile/Profile'
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path='/' component={Dashboard} />
+      <Route exact path='/activity' component={Activity} />
+      <Route exact path='/destinations' component={Appointments} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/profie' component={Profile}/>
+      <Route exact path='/services' component={Services} />
+      <Route exact path='/contacts' component={Contacts} />
+    </Switch>
+  )
+}
+
+export default Routes
