ad241709fd70a36d6640fa59d3553643abdc29e6
Denis Kolaj <deniskolaj2@gmail.com>
2021-05-01 15:48:30 +0200

    krijova nje component te re per Login Page

---
+++ src/Components/LoginPage/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function Login() {
+  return (
+    <div className="login-page">
+      <div className="login-content-container">
+        <input type="text"></input>
+        <input type="password"></input>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
