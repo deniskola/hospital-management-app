d5858be98042f150d5ba14c6429b04e9bc752078
Atlantik15 <atlantik.jashari@gmail.com>
2021-05-02 19:25:44 +0200

    krijova nje pjese te aktivitetit

--- src/Components/MainView/Activity/Activity.js
+++ src/Components/MainView/Activity/Activity.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import ActivityContainer from './Activity.styles';
 
 const Activity = () => {
-  return <h1>Activity Page</h1>
+  return (
+    <ActivityContainer>
+      <h1 class="aktiviteti-titull">Activity</h1>
+      <div class="container">
+        
+  <div class="box">
+    <div class="circle"></div>
+    
+  </div>
+  <div class="box">
+  <div class="circle"></div>
+  </div>
+  <div class="box">
+  <div class="circle"></div>
+  </div>
+  <div class="box">
+  <div class="circle"></div>
+  </div>
+</div>
+     
+      
+    </ActivityContainer>
+  )
 }
 
 export default Activity
