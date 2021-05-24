a172eb9e5dacf8f7bc4384f4f585f2d7c0e4505a
Denis Kolaj <deniskolaj2@gmail.com>
2021-05-25 01:39:50 +0200

    created login page structure and style

--- src/App.js
+++ src/App.js
@@ -1,32 +1,66 @@
-import React from 'react';
-import * as s from './App.styles';
-import * as Palette from './colors'
+import React from "react";
+import * as s from "./App.styles";
+import * as Palette from "./colors";
 
 // Components
-import Sidebar from './Components/Sidebar/Sidebar';
-import MainView from './Components/MainView/MainView'
-
+import Sidebar from "./Components/Sidebar/Sidebar";
+import MainView from "./Components/MainView/MainView";
+import Login from "./Components/LoginPage/Login";
 
 const App = () => {
-
   const sidebarHeader = {
-    fullName: 'Hospital X',
-    shortName: 'X'
+    fullName: "Hospital X",
+    shortName: "X",
   };
   const menuItems = [
-    {name: 'Dashboard', to: '/', icon: '/icons/dashboard.png', subMenuItems: [] },
-    {name: 'Activity', to: '/activity', icon: '/icons/activity.png', subMenuItems: [] },
-    {name: 'Appointments', to: '/appointments', icon: '/icons/appointment.png', subMenuItems: [] },
-    {name: 'Profile',to:'/profile',icon:'/icons/profile.png',subMenuItems:[]},
-    {name: 'About', to: '/about', icon: '/icons/aboutus.png', subMenuItems: [] },
-    {name: 'Services', to: '/services', icon: '/icons/services.png', subMenuItems: [] },
-    {name: 'Contact Us', to: '/contacts', icon: '/icons/contactus.png', subMenuItems: [] }
+    {
+      name: "Dashboard",
+      to: "/",
+      icon: "/icons/dashboard.png",
+      subMenuItems: [],
+    },
+    {
+      name: "Activity",
+      to: "/activity",
+      icon: "/icons/activity.png",
+      subMenuItems: [],
+    },
+    {
+      name: "Appointments",
+      to: "/appointments",
+      icon: "/icons/appointment.png",
+      subMenuItems: [],
+    },
+    {
+      name: "Profile",
+      to: "/profile",
+      icon: "/icons/profile.png",
+      subMenuItems: [],
+    },
+    {
+      name: "About",
+      to: "/about",
+      icon: "/icons/aboutus.png",
+      subMenuItems: [],
+    },
+    {
+      name: "Services",
+      to: "/services",
+      icon: "/icons/services.png",
+      subMenuItems: [],
+    },
+    {
+      name: "Contact Us",
+      to: "/contacts",
+      icon: "/icons/contactus.png",
+      subMenuItems: [],
+    },
   ];
 
   const fonts = {
-    header: 'ZCOOL KuaiLe',
-    menu: 'Poppins'
-  }
+    header: "ZCOOL KuaiLe",
+    menu: "Poppins",
+  };
 
   return (
     <s.App>
@@ -39,6 +73,6 @@ const App = () => {
       <MainView />
     </s.App>
   );
-}
+};
 
 export default App;
