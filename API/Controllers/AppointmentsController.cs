32bb4783044080625099f04796a6a506ab8d3da0
Denis Kolaj <deniskolaj2@gmail.com>
2021-06-12 14:57:11 +0200

    user login/register backend logic

--- API/Controllers/AppointmentsController.cs
+++ API/Controllers/AppointmentsController.cs
@@ -8,12 +8,14 @@ using System;
 using MediatR;
 using Application.AddAppointments;
 using System.Threading;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class AppointmentsController : BaseApiController
     {
-       
+
 
         [HttpGet]
         public async Task<ActionResult<List<AddAppointment>>> GetAppointments()
@@ -24,26 +26,26 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<AddAppointment>> GetAppointments(int id)
         {
-            return await Mediator.Send(new Details.Query{Id = id});
+            return await Mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateAppointment(AddAppointment appointments)
         {
-            return Ok(await Mediator.Send(new Create.Command {AddAppointment = appointments}));
+            return Ok(await Mediator.Send(new Create.Command { AddAppointment = appointments }));
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> EditAppointment(int id, AddAppointment appointment)
         {
             appointment.Id = id;
-            return Ok(await Mediator.Send(new Edit.Command{AddAppointment = appointment}));
+            return Ok(await Mediator.Send(new Edit.Command { AddAppointment = appointment }));
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(int id)
         {
-            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
         }
     }
 }
--- API/Controllers/PAllergiesController.cs
+++ API/Controllers/PAllergiesController.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using Domain;
 using System;
 using Application.ProfileA;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class PAllergiesController : BaseApiController
     {
 
@@ -19,26 +21,26 @@ namespace API.Controllers
         [HttpGet("{Id}")]
         public async Task<ActionResult<PAllergies>> GetAllergies(Guid id)
         {
-            return await Mediator.Send(new DetailsA.Query{Id=id});
+            return await Mediator.Send(new DetailsA.Query { Id = id });
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateAllergies(PAllergies pAllergies)
         {
-            return Ok(await Mediator.Send(new CreateAllergies.Command{PAllergies=pAllergies}));
+            return Ok(await Mediator.Send(new CreateAllergies.Command { PAllergies = pAllergies }));
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> EditAllergies (Guid id,PAllergies pAllergies)
+        public async Task<IActionResult> EditAllergies(Guid id, PAllergies pAllergies)
         {
-            pAllergies.Id=id;
-            return Ok(await Mediator.Send(new EditAllergies.Command{PAllergies=pAllergies}));
+            pAllergies.Id = id;
+            return Ok(await Mediator.Send(new EditAllergies.Command { PAllergies = pAllergies }));
         }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAllergies (Guid id)
+        public async Task<IActionResult> DeleteAllergies(Guid id)
         {
-            return Ok(await Mediator.Send(new DeleteAllergies.Command{Id=id}));
+            return Ok(await Mediator.Send(new DeleteAllergies.Command { Id = id }));
         }
     }
 }
