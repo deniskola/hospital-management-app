13d8a3d2c61efeaf701c4989820464e1c4ede57e
Denis Kolaj <deniskolaj2@gmail.com>
2021-07-13 07:20:33 +0200

    fixed an error

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,3 +1,6 @@
+using System.Text.Json.Serialization;
+using Application.UserCore;
+using Application.Users;
 using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -5,43 +8,42 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Persistence;
-using System.Text.Json.Serialization;
 
 namespace API.Extensions
 {
     public static class ApplicationServiceExtensions
     {
         public static IServiceCollection AddAplicationServices(this IServiceCollection services,
-        IConfiguration config){
+        IConfiguration config)
+        {
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
 
-            services.AddDbContext<DataContext>(opt =>{
+            services.AddDbContext<DataContext>(opt =>
+            {
                 opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
             });
-            services.AddCors(opt =>{
-                opt.AddPolicy("CorsPolicy",policy=>{
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
                     policy.AllowAnyMethod()
                     .AllowAnyHeader()
                     .WithOrigins("http://localhost:3000");
                 });
             });
-            
 
-            /*
+
+
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
-            
-            services.AddControllers().AddJsonOptions(x =>{
-                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-                x.JsonSerializerOptions.PropertyNamingPolicy = null;
-                }
-            );*/
 
-            
+
+
+
 
             return services;
         }
